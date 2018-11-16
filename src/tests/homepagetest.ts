61445146f6ef061cc170fb1d2b917ba3e6877200
Asad Hasan <asadhasan180@hotmail.com>
2018-11-16 14:38:30 +0000

    updated gitignore and introduced hooks

--- src/tests/homepagetest.ts
+++ src/tests/homepagetest.ts
@@ -3,14 +3,17 @@ import {Builder, Capabilities, ThenableWebDriver, WebElement} from "selenium-web
 import {Homepage} from "../pages/homepage";
 
 describe("home button on homepage", (): void => {
+    let driver: ThenableWebDriver;
+
+    before(() => driver = new Builder()
+        .withCapabilities(Capabilities.chrome())
+        .build());
     it("should return results", async (): Promise<void> => {
-        const driver: ThenableWebDriver = new Builder()
-            .withCapabilities(Capabilities.chrome())
-            .build();
         const homepage: Homepage = new Homepage(driver);
         const homes: Promise<WebElement[]> = homepage.open()
             .then((value) => value.getHomes())
             .then((value) => value.getResults());
         await expect(await homes).to.not.be.empty;
     });
+    after(async (): Promise<void> => await driver.quit());
 });
