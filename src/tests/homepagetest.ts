5ee62daa1a798502db438650742ae1989379c62a
Asad Hasan <asadhasan180@hotmail.com>
2018-11-22 10:56:11 +0000

    updated test to take screenshot on failure

--- src/tests/homepagetest.ts
+++ src/tests/homepagetest.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import "chromedriver";
 import { Builder, ThenableWebDriver, WebElement } from "selenium-webdriver";
 
+import * as fs from "fs";
 import { Homepage } from "../pages/homepage";
 import { SearchResults } from "../pages/searchresults";
 
@@ -14,6 +15,18 @@ describe("'homes' button on homepage", (): void => {
     const searchResults: SearchResults = await homepage.getHomes();
     const homes: WebElement[] = await searchResults.getResults();
     await expect(homes).to.not.be.empty;
+  });
+  afterEach(async function() {
+    if (this.currentTest !== undefined) {
+      const testName: string = this.currentTest.title;
+      const testState = this.currentTest.state;
+
+      if (testState === "failed") {
+        const screenshot: string = await driver.takeScreenshot();
+        const screenshotPath: string = `TestResults/Screenshots/${testName}.png`;
+        fs.writeFileSync(screenshotPath, screenshot, "base64");
+      }
+    }
   });
   after(async (): Promise<void> => driver.quit());
 });
